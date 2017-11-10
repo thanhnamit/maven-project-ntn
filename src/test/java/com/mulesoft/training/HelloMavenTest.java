6409926712041719cddbf5d7bf3314a9da283769
Nam Nguyen <thanhnam.it@gmail.com>
2017-11-10 11:59:22 +1100

    First commit

---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,20 @@
+package com.mulesoft.training;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+    @Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+        runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+    @Override
+    protected String getConfigFile() {
+        return "maven-project.xml";
+    }
+
+}
